17b84610f4c43b660ea37a5782544d14ecca548a
Lev Pikulev <44238774+pikulevlv@users.noreply.github.com>
2021-03-19 10:19:16 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-#if we saved token in environmens as BOT_TOKEN, then:
-# import os
-# TOKEN = os.environ.get("BOT_TOKEN") # better to safe in untracked file .env
-
-TOKEN = "1455950795:AAHJHRwA5opkn0aKC6hDgC5vNSsXyWWvOKQ"
-# name = "forbusinessdemobot"
